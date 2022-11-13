bc691103a10a5e48b40d213716c507e6ac1cc019
MohamedSaleh <mohamed@startpoint.com>
2022-11-13 12:11:33 +0300

    v 1.0

--- admin/connect.php
+++ admin/connect.php
@@ -1,11 +1,11 @@
 <?php
 $servername = 'localhost';
-//$username = 'u191962684_topaz';
-//$password = 'QnTOR7zDOk8mgJ1!';
-//$dbname = 'u191962684_topaz';
- $username = 'root';
- $password = '';
- $dbname = 'projectdb';
+$username = 'u191962684_topaz';
+$password = 'QnTOR7zDOk8mgJ1!';
+$dbname = 'u191962684_topaz';
+// $username = 'root';
+// $password = '';
+// $dbname = 'projectdb';
 $conn=new mysqli($servername ,$username ,$password ,$dbname ) or die("failing to connect to the DB");
 $conn->set_charset('utf8');
 ?>
